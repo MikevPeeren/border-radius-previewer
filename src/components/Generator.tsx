19f6b5598896115d153a92da63bf5caedac9596a
MikevPeeren <MikevPeeren@hotmail.com>
2020-05-25 09:17:45 +0200

    Added Shape

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -5,7 +5,11 @@ import React from 'react';
 import './Generator.css';
 
 const Generator = () => {
-  return <div className="borderRadiusPreviewer"></div>;
+  return (
+    <div className="borderRadiusPreviewer">
+      <div className="borderRadiusPreviewer__shape"></div>
+    </div>
+  );
 };
 
 export default Generator;
