904486b84749514735b9465659d9f6f748f64f9f
MikevPeeren <MikevPeeren@hotmail.com>
2020-05-26 14:52:40 +0200

    Added Sliders

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -8,6 +8,10 @@ const Generator = () => {
   return (
     <div className="borderRadiusPreviewer">
       <div className="borderRadiusPreviewer__shape"></div>
+      <span className="borderRadiusPreviewer__handle" id="top"></span>
+      <span className="borderRadiusPreviewer__handle" id="left"></span>
+      <span className="borderRadiusPreviewer__handle" id="right"></span>
+      <span className="borderRadiusPreviewer__handle" id="bottom"></span>
     </div>
   );
 };
