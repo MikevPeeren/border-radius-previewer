4d1707e33b5679b360b08084c852cd664970c653
MikevPeeren <MikevPeeren@hotmail.com>
2020-05-26 17:00:53 +0200

    Adjusted Sliders

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,17 +1,91 @@
 // React
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // CSS
 import './Generator.scss';
 
 const Generator = () => {
+  const [topSliderValue, setTopSliderValue] = useState(50);
+  const [leftSliderValue, setLeftSliderValue] = useState(50);
+  const [rightSliderValue, setRightSliderValue] = useState(50);
+  const [bottomSliderValue, setBottomSliderValue] = useState(50);
+
+  const handleTopSliderChange = (event: any) => {
+    console.log(event.target.value);
+    //@ts-ignore
+    setTopSliderValue(event.target.value);
+  };
+
+  const handleLeftSliderChange = (event: any) => {
+    console.log(event.target.value);
+    //@ts-ignore
+    setLeftSliderValue(event.target.value);
+  };
+
+  const handleRightSliderChange = (event: any) => {
+    console.log(event.target.value);
+    //@ts-ignore
+    setRightSliderValue(event.target.value);
+  };
+
+  const handleBottomSliderChange = (event: any) => {
+    console.log(event.target.value);
+    //@ts-ignore
+    setBottomSliderValue(event.target.value);
+  };
+
   return (
     <div className="borderRadiusPreviewer">
       <div className="borderRadiusPreviewer__shape"></div>
-      <span className="borderRadiusPreviewer__handle" id="top"></span>
+      {topSliderValue && (
+        <input
+          className="borderRadiusPreviewer__handle"
+          id="top"
+          type="range"
+          min="0"
+          max="100"
+          value={topSliderValue}
+          onChange={handleTopSliderChange}
+        />
+      )}
+      {leftSliderValue && (
+        <input
+          className="borderRadiusPreviewer__handle"
+          id="left"
+          type="range"
+          min="0"
+          max="100"
+          value={leftSliderValue}
+          onChange={handleLeftSliderChange}
+        />
+      )}
+      {rightSliderValue && (
+        <input
+          className="borderRadiusPreviewer__handle"
+          id="right"
+          type="range"
+          min="0"
+          max="100"
+          value={rightSliderValue}
+          onChange={handleRightSliderChange}
+        />
+      )}
+      {bottomSliderValue && (
+        <input
+          className="borderRadiusPreviewer__handle"
+          id="bottom"
+          type="range"
+          min="0"
+          max="100"
+          value={bottomSliderValue}
+          onChange={handleBottomSliderChange}
+        />
+      )}
+
+      {/* <span className="borderRadiusPreviewer__handle" id="top"></span>
       <span className="borderRadiusPreviewer__handle" id="left"></span>
       <span className="borderRadiusPreviewer__handle" id="right"></span>
-      <span className="borderRadiusPreviewer__handle" id="bottom"></span>
+      <span className="borderRadiusPreviewer__handle" id="bottom"></span> */}
     </div>
   );
 };
