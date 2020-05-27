7a71d0f815e7ed3b6f4fcdf20cb5ba5a568cba9f
MikevPeeren <MikevPeeren@hotmail.com>
2020-05-27 16:07:07 +0200

    Added Slider Functionality

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,5 +1,5 @@
 // React
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // CSS
 import './Generator.scss';
@@ -31,9 +31,112 @@ const Generator = () => {
     }
   };
 
+  // TODO: Cleanup the mess below
+
+  // Handle Top Slider
+  useEffect(() => {
+    const shapeElement = document.getElementById('shape');
+
+    if (shapeElement) {
+      const topLeftRadius = shapeElement.style.borderTopLeftRadius;
+      const topRightRadius = shapeElement.style.borderTopLeftRadius;
+
+      const secondPercentageTopLeftRadius =
+        topLeftRadius.charAt(topLeftRadius.length - 4) === '%'
+          ? topLeftRadius.substring(topLeftRadius.length - 5)
+          : topLeftRadius.substring(topLeftRadius.length - 4);
+      const secondPercentageTopRightRadius =
+        topRightRadius.charAt(topLeftRadius.length - 4) === '%'
+          ? topRightRadius.substring(topLeftRadius.length - 5)
+          : topRightRadius.substring(topLeftRadius.length - 4);
+
+      shapeElement.style.borderTopLeftRadius = `${topSliderValue}% ${secondPercentageTopLeftRadius}`;
+      shapeElement.style.borderTopRightRadius = `${
+        100 - topSliderValue
+      }% ${secondPercentageTopRightRadius}`;
+    }
+  }, [topSliderValue]);
+
+  // Handle Bottom Slider
+  useEffect(() => {
+    const shapeElement = document.getElementById('shape');
+
+    if (shapeElement) {
+      const bottomLeftRadius = shapeElement.style.borderBottomLeftRadius;
+      const bottomRightRadius = shapeElement.style.borderBottomRightRadius;
+
+      const secondPercentageBottomLeftRadius =
+        bottomLeftRadius.charAt(bottomLeftRadius.length - 4) === '%'
+          ? bottomLeftRadius.substring(bottomLeftRadius.length - 5)
+          : bottomLeftRadius.substring(bottomLeftRadius.length - 4);
+      const secondPercentageBottomRightRadius =
+        bottomRightRadius.charAt(bottomRightRadius.length - 4) === '%'
+          ? bottomRightRadius.substring(bottomRightRadius.length - 5)
+          : bottomRightRadius.substring(bottomRightRadius.length - 4);
+
+      shapeElement.style.borderBottomLeftRadius = `${bottomSliderValue}% ${secondPercentageBottomLeftRadius}`;
+      shapeElement.style.borderBottomRightRadius = `${
+        100 - bottomSliderValue
+      }% ${secondPercentageBottomRightRadius}`;
+    }
+  }, [bottomSliderValue]);
+
+  // Handle Left Slider
+  useEffect(() => {
+    const shapeElement = document.getElementById('shape');
+
+    if (shapeElement) {
+      const topLeftRadius = shapeElement.style.borderTopLeftRadius;
+      const bottomLeftRadius = shapeElement.style.borderBottomLeftRadius;
+
+      const firstPercentageTopLeftRadius =
+        topLeftRadius.charAt(0) === '0'
+          ? topLeftRadius.substring(0, 3)
+          : topLeftRadius.substring(0, 4);
+
+      const firstPercentageBottomLeftRadius =
+        bottomLeftRadius.charAt(0) === '0'
+          ? bottomLeftRadius.substring(0, 3)
+          : bottomLeftRadius.substring(0, 4);
+
+      shapeElement.style.borderTopLeftRadius = `${firstPercentageTopLeftRadius} ${
+        100 - leftSliderValue
+      }%`;
+      shapeElement.style.borderBottomLeftRadius = `${firstPercentageBottomLeftRadius}
+      ${leftSliderValue}%`;
+    }
+  }, [leftSliderValue]);
+
+  // Handle Right Slider
+  useEffect(() => {
+    const shapeElement = document.getElementById('shape');
+
+    if (shapeElement) {
+      const topRightRadius = shapeElement.style.borderTopRightRadius;
+      const bottomRightRadius = shapeElement.style.borderBottomRightRadius;
+
+      const firstPercentageTopRightRadius =
+        topRightRadius.charAt(0) === '0'
+          ? topRightRadius.substring(0, 3)
+          : topRightRadius.substring(0, 4);
+      const firstPercentageBottomRightRadius =
+        bottomRightRadius.charAt(0) === '0'
+          ? bottomRightRadius.substring(0, 3)
+          : bottomRightRadius.substring(0, 4);
+
+      console.log(firstPercentageTopRightRadius);
+
+      shapeElement.style.borderTopRightRadius = `${firstPercentageTopRightRadius} ${
+        100 - rightSliderValue
+      }%`;
+      shapeElement.style.borderBottomRightRadius = `${firstPercentageBottomRightRadius}
+      ${rightSliderValue}%`;
+    }
+  }, [rightSliderValue]);
+
   return (
     <div className="borderRadiusPreviewer">
-      <div className="borderRadiusPreviewer__shape"></div>
+      <div id="shape" className="borderRadiusPreviewer__shape"></div>
       {topSliderValue && (
         <input
           className="borderRadiusPreviewer__handle"
